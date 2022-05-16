8662034c876441994a3dc733dc0580df199519e6
Michael Lambert <m.lambert@rocketlab.co.nz>
2022-05-16 20:06:15 +1200

    Build commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='file_management',
+    version='0.1',
+    packages=['file_management'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=['tkinter'],
+)
