0f69c8a80cbc52757f35831e1b4fa7c348f373a2
Michael Lambert <m.lambert@rocketlab.co.nz>
2022-05-17 12:58:00 +1200

    added function for selecting multiple files

--- file_management/file_management.py
+++ file_management/file_management.py
@@ -4,14 +4,12 @@ Created on Mon May 16 16:51:21 2022
 
 @author: m.lambert
 """
-
 import tkinter as tk
 from tkinter import filedialog
 
-
 def browse_for_file():
     """
-    Use tkinter to browse for the file
+    Use tkinter to browse for a file
 
     Returns
     -------
@@ -20,8 +18,31 @@ def browse_for_file():
 
     """
     root = tk.Tk()
+    root.title('Choose a file')
     root.withdraw() # use to hide tkinter window
+    root.attributes("-topmost", True)
+    root.after_idle(root.attributes,'-topmost',False)
     
     fpath = filedialog.askopenfilename()
     
     return fpath
+
+def browse_for_files():
+    """
+    Use tkinter to browse for a file
+
+    Returns
+    -------
+    fpaths : tuple
+        Tuple of filepath strings
+
+    """
+    root = tk.Tk()
+    root.title('Choose a file')
+    root.withdraw() # use to hide tkinter window
+    root.attributes("-topmost", True)
+    root.after_idle(root.attributes,'-topmost',False)
+    
+    fpaths = filedialog.askopenfilenames()
+    
+    return fpaths
--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
     name='file_management',
     version='0.1',
     packages=['file_management'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires=['tkinter'],
 )
